4e8732dbcda6cac3ab78125de561faf53c77b5f5
iGrek <59765393+iGrekInLapland@users.noreply.github.com>
2021-02-27 16:20:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function toReadable (number) {
-  
+    if(number===0)return 'zero';
+    let units, tens, hundreds, res, support,  space;
+    units = number % 10;
+    tens = Math.floor((number % 100)/10 );
+    hundreds = Math.floor(number / 100 );
+
+    let base = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let t = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+    support = units + tens *10;
+
+    if((9 < support) && (support < 20)) units = support;
+
+    hundreds = ((hundreds === 0)? '':`${base[hundreds]} hundred `);
+    res = hundreds + t[tens] + base[units];
+     return res.trim();
 }
